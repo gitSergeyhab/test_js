53f9845002dda1b63a22c05731a53eea8d93e00a
Sergey <redmoloch@yanadex.ru>
2020-11-13 18:49:34 +0300

    test commit

---
+++ index.js
@@ -0,0 +1,19 @@
+setTimeout(wake, 5000);
+function wake() {
+    alert("AUUUU!!!!!!");
+}
+
+// const ans = prompt("write something", "something")
+
+
+// alert(ans)
+// alert(ans + typeof(ans))
+
+const anss = [];
+anss[0] = prompt("name?", "");
+anss[1] = prompt("last name?", "");
+anss[2] = prompt("age?" , "");
+
+// alert(anss);
+// document.write(anss);
+console.log(anss)
